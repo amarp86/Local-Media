6be45adc32d6991c61b0be687f99aca8173d4ddd
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-02-18 09:20:17 -0500

    styled footer

--- local-media/src/components/Footer.jsx
+++ local-media/src/components/Footer.jsx
@@ -1,5 +1,14 @@
+import Octocat from "../images/Octocat.png";
+import Linkedin from "../images/linkedinlogo.png";
+
 function Footer(props) {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <img src={Octocat} alt="github" className="footer-img" />
+      <img src={Linkedin} alt="linkedin" className="footer-img" />
+      <p>Copyright 2021 Amar Patel</p>
+    </div>
+  );
 }
 
 export default Footer;
