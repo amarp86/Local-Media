efabc3f840adc325e0cd3e66d5d2c345aadf5042
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-02-18 09:26:18 -0500

    finished footer

--- local-media/src/components/Footer.jsx
+++ local-media/src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import Octocat from "../images/Octocat.png";
 import Linkedin from "../images/linkedinlogo.png";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
   return (
     <div className="footer">
-      <img src={Octocat} alt="github" className="footer-img" />
-      <img src={Linkedin} alt="linkedin" className="footer-img" />
+      <Link to={{ pathname: "https://github.com/amarp86" }} target="_blank">
+        <img src={Octocat} alt="github" className="footer-img" />
+      </Link>
+      <Link
+        to={{ pathname: "https://www.linkedin.com/in/amar-patel-a90a58b4/" }}
+        target="_blank"
+      >
+        <img src={Linkedin} alt="linkedin" className="footer-img" />
+      </Link>
       <p>Copyright 2021 Amar Patel</p>
     </div>
   );
